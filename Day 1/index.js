1223ab5d056540a2e15c5d921c1232eb7a0d79f0
Durgesh tiwari <durgeshtiwari362592@gmail.com>
2024-07-13 11:34:02 +0530

    Day_1 Task Completed

---
+++ Day 1/index.js
@@ -0,0 +1,31 @@
+var variable_1 = 4112;
+console.log(variable_1);
+let variable_2 = "Chai aur Code";
+console.log(variable_2);
+
+const variable_3 = true;
+console.log(variable_3);
+
+let num = 2114;
+console.log(typeof num);
+let str = "Hitesh Sir"
+console.log(typeof str);
+let bool = true;
+console.log(typeof bool);
+const arr = ["JavaScript", "C++", "Python", "C#"];
+console.log(typeof arr);
+let obj = {
+  name:"Durgesh Tiwari",
+  age:21,
+  Are_You_Student:true,
+}
+console.log(typeof obj);
+
+let variable_4 = "React";
+console.log(variable_4);
+variable_4 = "Angular";
+console.log(variable_4);
+
+const variable_5 = "JavaScript";
+variable_5 = "Python";
+console.log(variable_5)
