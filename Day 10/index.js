e596031382aba422c7fc73dc0560b343f5ee7531
Durgesh tiwari <durgeshtiwari362592@gmail.com>
2024-07-22 15:37:49 +0530

    Day 10 Task completed

--- Day 10/index.js
+++ Day 10/index.js
@@ -39,4 +39,15 @@ form.addEventListener("submit",(event)=>{
   console.log(textField.value);
 })
 
-//9
+//9,10
+const customUI = document.createElement('ul');
+
+for (var i = 1; i <= 10; i++) {
+    const newElement = document.createElement('li');
+    newElement.textContent = "This is line " + i;
+    newElement.addEventListener('click', () => {
+        console.log('Responding')
+    })
+    customUI.appendChild(newElement);
+}
+document.body.appendChild(customUI);
