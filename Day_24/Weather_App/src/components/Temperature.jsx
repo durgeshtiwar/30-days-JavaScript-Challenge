5e70b68576ec691069bc440f29918fde69b5251f
Durgesh Tiwari <112402246+durgeshtiwar@users.noreply.github.com>
2024-09-23 12:23:14 +0000

    Api Fetched

--- Day_24/Weather_App/src/components/Temperature.jsx
+++ Day_24/Weather_App/src/components/Temperature.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 
 function Temperature() {
     const findWeather = async()=>{
-        const url = 'https://www.visualcrossing.com/weather-api'
-        const responce = await fetch(url);
-        const data = await responce.json();
-        console.log(data.posts[0]);
+    const responce = await fetch("https://api.weatherapi.com/v1/current.json?key=b16ce6c940c14817a26113929242309&q=India")
+    const data = await responce.json();
+    console.log(data["location"]);
+    console.log(data.current.temp_c);
     }
+    //console.log(findWeather());
     findWeather();
+    
   return (
     <div className='h-100 w-60 p-4  bg-green-200'> 
         <div className="w-full h-10 p-4   border-2 border-gray-300 bg-white shadow-lg rounded-lg">
