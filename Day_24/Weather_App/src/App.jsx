bb12905b62779a6714dc158601daf12ac0c9ee12
Durgesh Tiwari <112402246+durgeshtiwar@users.noreply.github.com>
2024-09-24 10:35:11 +0000

    Api fetching

--- Day_24/Weather_App/src/App.jsx
+++ Day_24/Weather_App/src/App.jsx
@@ -1,12 +1,26 @@
 import './App.css'
 import Temperature from './components/Temperature'
 
-function App() {
-
-  return (
+function App() {  
+    let items = null;
+    const findWeather = async()=>{
+    try {
+      const responce = await fetch("https://api.weatherapi.com/v1/current.json?key=b16ce6c940c14817a26113929242309&q=India")
+      const data = await responce.json();
+      const assignData = () =>
+      {
+        items = data;
+      }
+      assignData();
+      
+    } catch (error) {
+      console.log(error.message);
+    }}
+    findWeather();
+    console.log(items)
+    return (
     <>
-    <h1 className='text-center text-3xl font-bold'>Weather App</h1>
-    <Temperature/>
+    <Temperature items ={items} />
     </>
   )
 }
--- Day_24/Weather_App/src/components/Temperature.jsx
+++ Day_24/Weather_App/src/components/Temperature.jsx
@@ -1,23 +1,15 @@
 import React from 'react'
 
-function Temperature() {
-    const findWeather = async()=>{
-    const responce = await fetch("https://api.weatherapi.com/v1/current.json?key=b16ce6c940c14817a26113929242309&q=India")
-    const data = await responce.json();
-    console.log(data["location"]);
-    console.log(data.current.temp_c);
-    }
-    //console.log(findWeather());
-    findWeather();
-    
+function Temperature({items}) {
+  console.log(items);
   return (
-    <div className='h-100 w-60 p-4  bg-green-200'> 
-        <div className="w-full h-10 p-4   border-2 border-gray-300 bg-white shadow-lg rounded-lg">
-            <h1>City Name</h1>
+    <div className='h-100 w-60 p-4'> 
+        <div className="w-full h-10  border-2 border-gray-300 shadow-lg rounded-lg bg-blue-900 text-white">
+          <h1 className='text-center pt-1 font-bold'></h1>
         </div>
-        <div className="w-full h-40 p-4  border-2 border-gray-300 bg-white shadow-lg rounded-lg">
-            <h1>Temperature</h1>
-            <h1>Condition</h1>
+        <div className="w-full h-36 border-2 border-gray-300 bg-white shadow-lg rounded-lg text-center">
+            <h1 className='font-bold p-2'>Temperature : </h1>
+            <h1 className='font-bold '>Condition</h1>
         </div>
     </div>
   )
