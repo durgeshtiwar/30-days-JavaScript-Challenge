04786363f347c502bec2a7d75a16ee37017c08c9
Durgesh Tiwari <112402246+durgeshtiwar@users.noreply.github.com>
2024-09-24 13:01:35 +0000

    Functionality add

--- Day_24/Weather_App/src/App.jsx
+++ Day_24/Weather_App/src/App.jsx
@@ -1,28 +1,26 @@
+import { useEffect, useState } from 'react'
 import './App.css'
 import Temperature from './components/Temperature'
 
 function App() {  
-    let items = null;
-    const findWeather = async()=>{
+    const [data, setData] = useState();
+    const findWeather = async() =>{
     try {
-      const responce = await fetch("https://api.weatherapi.com/v1/current.json?key=b16ce6c940c14817a26113929242309&q=India")
-      const data = await responce.json();
-      const assignData = () =>
-      {
-        items = data;
-      }
-      assignData();
-      
+      const responce = await fetch("https://api.weatherapi.com/v1/current.json?key=b16ce6c940c14817a26113929242309&q=surat")
+      const items = await responce.json();
+      const result = [items];
+      setData(result);
     } catch (error) {
       console.log(error.message);
     }}
-    findWeather();
-    console.log(items)
+    useEffect(()=>{
+      findWeather();
+    },[]);
     return (
-    <>
-    <Temperature items ={items} />
-    </>
-  )
-}
-
+      <>
+      <h1 className='text-center text-3xl font-bold'>Weather App
+      </h1>
+      <Temperature data = {data}/>
+      </>
+  )}
 export default App
--- Day_24/Weather_App/src/components/Temperature.jsx
+++ Day_24/Weather_App/src/components/Temperature.jsx
@@ -1,15 +1,19 @@
 import React from 'react'
 
-function Temperature({items}) {
-  console.log(items);
+function Temperature({data}) {
   return (
-    <div className='h-100 w-60 p-4'> 
+    <div className='h-60 w-60 p-4 '> 
         <div className="w-full h-10  border-2 border-gray-300 shadow-lg rounded-lg bg-blue-900 text-white">
-          <h1 className='text-center pt-1 font-bold'></h1>
+          <h1 className='text-center pt-1 font-bold'>{data && data.map(item => item.location.name)}</h1>
         </div>
-        <div className="w-full h-36 border-2 border-gray-300 bg-white shadow-lg rounded-lg text-center">
-            <h1 className='font-bold p-2'>Temperature : </h1>
-            <h1 className='font-bold '>Condition</h1>
+        <div className="w-full h-44 border-2 border-gray-300 bg-white shadow-lg rounded-lg pl-3">
+            <h1 className='font-bold pl-4 pt-1'>Temperature : {data && data.map(item => item.current.temp_c)}</h1>
+            <h1 className='font-bold pl-7 '>Condition :{data && data.map(item=><p key={Date.now()}>{item.current.condition.text}</p>)}
+            {data && data.map(item => 
+              <img key={Date.now} src={`${item.current.condition.icon}`}></img>
+            )}
+            {data && data.map(item => item.current.condition.code)}
+            </h1>
         </div>
     </div>
   )
