d40e7ecc9e7179650c36cdd02f6297264870e77b
Durgesh Tiwari <112402246+durgeshtiwar@users.noreply.github.com>
2024-09-26 10:47:29 +0000

    Movie search App Started

---
+++ Day_25/Movie_Search_App/src/App.jsx
@@ -0,0 +1,13 @@
+
+function App() {
+
+  return (
+    <center>
+      <h1 className="text-3xl font-bold underline">
+      Movie Search App
+      </h1>
+    </center>
+  )
+}
+
+export default App
