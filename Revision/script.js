1456cc0373a01b97243e385b00c745525729b7d8
Durgesh tiwari <durgeshtiwari362592@gmail.com>
2025-01-27 14:07:58 +0530

    Events handling

--- Revision/script.js
+++ Revision/script.js
@@ -15,7 +15,17 @@
 // btn.style.backgroundColor = "red";
 // btn.style.color = "white";
 
-let para = document.createElement("p");
-para.innerText = "My Name is Durgesh Tiwari";
-document.querySelector("body").prepend(para);
-para.classList.add("new_class");
+// let para = document.createElement("p");
+// para.innerText = "My Name is Durgesh Tiwari";
+// document.querySelector("body").prepend(para);
+// para.classList.add("new_class");
+
+
+let btn = document.querySelector(".toggle-btn");
+btn.addEventListener("click",(e)=>{
+  if (document.body.style.backgroundColor == "black") {
+    document.body.style.backgroundColor = "white";
+  } else {
+    document.body.style.backgroundColor = "black";
+  }
+})
